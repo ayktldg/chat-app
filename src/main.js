528239bd7a99c4ec3f1cb0df683303e06573daff
Aykut <aykutuludag26@gmail.com>
2021-02-26 19:43:19 +0200

    installed tailwind and firebase

--- src/main.js
+++ src/main.js
@@ -3,10 +3,41 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./assets/css/tailwind.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsSNibaYDFeGGBog7eyDWCipyIychKzh8",
+  authDomain: "chat-app-78874.firebaseapp.com",
+  projectId: "chat-app-78874",
+  storageBucket: "chat-app-78874.appspot.com",
+  messagingSenderId: "277088526062",
+  appId: "1:277088526062:web:c09b662b773f9a09339dff"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//const db = firebase.firestore();
+
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+let app;
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log("user", user);
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
+
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount("#app");
