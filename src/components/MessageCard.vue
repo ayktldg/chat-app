cbcb5a050344cecabdf69d66412156668e406b65
Aykut <aykutuludag26@gmail.com>
2021-03-11 13:07:59 +0200

    fixed message time's bug

--- src/components/MessageCard.vue
+++ src/components/MessageCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="messageLocation" class="w-3/4 md:w-auto md:max-w-md my-2 mx-4">
-    <div :class="messageBg" class=" rounded-3xl shadow-lg px-4 py-2">
+    <div :class="messageBg" class="rounded-3xl shadow-lg px-4 py-2">
       <p class="text-gray-200 break-words">
         {{ message.message }}
       </p>
@@ -41,7 +41,8 @@ export default {
       }
     },
     messageTime() {
-      const timeArr = this.message.time.toDate().toUTCString().split(" ");
+      const timeInMilis = this.message.time.seconds * 1000;
+      const timeArr = new Date(timeInMilis).toUTCString().split(" ");
       const formattedTime = timeArr.slice(1, 5).join(" ");
       return formattedTime;
     },
